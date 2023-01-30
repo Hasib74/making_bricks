c9d064d3fdd080da8800d5c24eff37ca3a7cae81
Hasib74 <hasibakon74@gmail.com>
2023-01-30 19:02:49 +0600

    * onek kicu

--- lib/core/dependencyInjection/app_dependency_injections.dart
+++ lib/core/dependencyInjection/app_dependency_injections.dart
@@ -1,3 +1,27 @@
+import '../../featchers/aktakicu/data/remoteDataSource/aktakicu_data_source.dart';
+import '../../featchers/aktakicu/data/respository/aktakicu_repository_impl.dart';
+import '../../featchers/aktakicu/domain/repository/aktakicu_repository.dart';
+
+import '../../featchers/aktakicu/domain/useCase/aktakicu_use_case.dart';
+
+import '../../featchers/aktakicu/presentation/functions/aktakicu_functions.dart';
+
+import '../../featchers/welcome/data/remoteDataSource/welcome_data_source.dart';
+import '../../featchers/welcome/data/respository/welcome_repository_impl.dart';
+import '../../featchers/welcome/domain/repository/welcome_repository.dart';
+
+import '../../featchers/welcome/domain/useCase/welcome_use_case.dart';
+
+import '../../featchers/welcome/presentation/functions/welcome_functions.dart';
+
+import '../../featchers/test/data/remoteDataSource/test_data_source.dart';
+import '../../featchers/test/data/respository/test_repository_impl.dart';
+import '../../featchers/test/domain/repository/test_repository.dart';
+
+import '../../featchers/test/domain/useCase/test_use_case.dart';
+
+import '../../featchers/test/presentation/functions/test_functions.dart';
+
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:gdn_health/core/flavor/app_flavor.dart';
 import 'package:gdn_health/core/network/app_network.dart';
@@ -38,4 +62,51 @@ Future intiGetIt() async {
   //usecase
   sl.registerLazySingleton<ProductUseCase>(
       () => ProductUseCase(producteRepository: sl()));
+
+//FK
+
+//function
+  sl.registerLazySingleton(() => TestFunctions());
+//DataSource
+  sl.registerLazySingleton<TestRemoteDataSource>(
+      () => TestRemoteDataSourceImpl());
+//repository
+  sl.registerLazySingleton<TestRepository>(
+      () => TestRepositoryImpl(testRemoteDataSource: sl()));
+//usecase
+  sl.registerLazySingleton<TestUseCase>(() => TestUseCase(eRepository: sl()));
+
+//function
+  sl.registerLazySingleton(() => WelcomeFunctions());
+//DataSource
+  sl.registerLazySingleton<WelcomeRemoteDataSource>(
+      () => WelcomeRemoteDataSourceImpl());
+//repository
+  sl.registerLazySingleton<WelcomeRepository>(
+      () => WelcomeRepositoryImpl(welcomeRemoteDataSource: sl()));
+//usecase
+  sl.registerLazySingleton<WelcomeUseCase>(
+      () => WelcomeUseCase(eRepository: sl()));
+
+//function
+  sl.registerLazySingleton(() => AktakicuFunctions());
+//DataSource
+  sl.registerLazySingleton<AktakicuRemoteDataSource>(
+      () => AktakicuRemoteDataSourceImpl());
+//repository
+  sl.registerLazySingleton<AktakicuRepository>(
+      () => AktakicuRepositoryImpl(aktakicuRemoteDataSource: sl()));
+//usecase
+  sl.registerLazySingleton<AktakicuUseCase>(
+      () => AktakicuUseCase(eRepository: sl()));
 }
+
+
+
+
+
+
+
+
+
+
--- lib/dcli.dart
+++ lib/dcli.dart
@@ -1,16 +1,70 @@
-import 'package:dcli/dcli.dart';
+import 'package:dcli/dcli.dart' as dcli;
+import 'package:gdn_health/core/extension/string_extendion.dart';
 
 main() async {
   runMason();
 }
 
-runMason() {
-  var name = ask('name:', required: true, validator: Ask.alpha);
+runMason() async {
+  var name = dcli.ask("name: ", required: true, validator: dcli.Ask.alpha);
+  //
+  print(dcli.green('Name of your features is: $name'));
 
-  "mason make my_feature featchers --name ${name} -o ./lib/featchers".run;
+  "mason make my_feature featchers --name $name -o ./lib/featchers".run;
 
-  "cd core".run;
-  "cd dependencyInjection".run;
-  "nano app_dependency_injections.dart".run;
-  "love".run;
+  _dependancyInjection(name);
 }
+
+_dependancyInjection(String name) {
+  var _file = "./lib/core/dependencyInjection/app_dependency_injections.dart";
+
+  var _data = dcli.read(_file);
+
+  // _data.toParagraph().replaceAll("}}\$", "     Love this \n}");
+
+  List<String> _splitedList = _data.toParagraph().split("}");
+
+  //_splitedList[_splitedList.length].append("FK");
+
+  String _writeData = "";
+
+  _splitedList.forEach((element) {
+    //  print("Index of ${_splitedList.indexOf(element)} $element  \n");
+    if (_splitedList.indexOf(element) == _splitedList.length - 1) {
+      //print("element : ${element}");
+
+      element.replaceAll("\n", "");
+      element.replaceAll(" ", "");
+
+      _writeData += element.replaceAll("}", "").replaceAll(" ", "") +
+          "//function \n sl.registerLazySingleton(() => ${capitalize(name)}Functions()); \n";
+
+      _writeData += element.replaceAll("}", "").replaceAll(" ", "") +
+          "//DataSource \n sl.registerLazySingleton<${capitalize(name)}RemoteDataSource>(() => ${capitalize(name)}RemoteDataSourceImpl()); \n";
+
+      _writeData += element.replaceAll("}", "").replaceAll(" ", "") +
+          "//repository \n   sl.registerLazySingleton<${capitalize(name)}Repository>(() => ${capitalize(name)}RepositoryImpl(${name.toLowerCase()}RemoteDataSource: sl()));\n";
+
+      _writeData += element.replaceAll("}", "").replaceAll(" ", "") +
+          "//usecase \n   sl.registerLazySingleton<${capitalize(name)}UseCase>(() => ${capitalize(name)}UseCase(eRepository: sl()));\n }";
+    } else {
+      if (_splitedList.indexOf(element) == _splitedList.length - 2) {
+        _writeData += element + " \n";
+      } else {
+        _writeData += element + " \n}";
+      }
+    }
+  });
+
+  String imports =
+      ''' import '../../featchers/${name.toLowerCase()}/data/remoteDataSource/${name.toLowerCase()}_data_source.dart'; 
+  import '../../featchers/${name.toLowerCase()}/data/respository/${name.toLowerCase()}_repository_impl.dart'; 
+  import '../../featchers/${name.toLowerCase()}/domain/repository/${name.toLowerCase()}_repository.dart'; \n
+  import '../../featchers/${name.toLowerCase()}/domain/useCase/${name.toLowerCase()}_use_case.dart'; \n
+  import '../../featchers/${name.toLowerCase()}/presentation/functions/${name.toLowerCase()}_functions.dart'; \n  
+  ''';
+
+  _file.write(imports + _writeData);
+}
+
+String capitalize(String s) => s[0].toUpperCase() + s.substring(1);
